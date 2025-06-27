df6a2ba16beea70bb080e13ac2fdc5be2e661432
A . Yasiru Nadeesha Aththanayaka <yasiru.20232389@iit.ac.lk>
2025-06-27 19:32:07 +0530

    Create HotelManager.java

---
+++ src/main/java/com/mycompany/hotelmanagementsystem/HotelManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.hotelmanagementsystem;
+
+/**
+ *
+ * @author b.villarini
+ */
+public interface HotelManager {
+    
+    boolean runMenu(); // Run Menu - main function
+    void addHotelStaff(); // Add hotel staff member
+    void printHotelStaffList(); // Print hotel staff member list
+    void addGuest(); // Add hotel guest
+    void runGUI(); // Run main GUI
+    
+}
